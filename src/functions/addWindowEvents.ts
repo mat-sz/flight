d1e4ac7344f0f31b769c4381983ebbac22cc5379
Mat Sz <contact@matsz.dev>
2019-12-07 12:16:19 +0100

    implemented touch screen support

--- src/functions/addWindowEvents.ts
+++ src/functions/addWindowEvents.ts
@@ -8,7 +8,7 @@ export default function addWindowEvents(camera: PerspectiveCamera, renderer: Web
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
-    })
+    });
 
     window.addEventListener('keydown', (event) => {
         const planeLane = store.getState().lane;
@@ -21,6 +21,19 @@ export default function addWindowEvents(camera: PerspectiveCamera, renderer: Web
                 if (planeLane < 1)
                     store.dispatch({ type: ActionType.SET_LANE, value: planeLane + 1 });
                 break;
+        }
+    });
+
+    window.addEventListener('touchstart', (event) => {
+        const planeLane = store.getState().lane;
+        const touch = event.touches[0];
+
+        if (touch.pageX < window.innerWidth/2) {
+            if (planeLane > -1)
+                store.dispatch({ type: ActionType.SET_LANE, value: planeLane - 1 });
+        } else {
+            if (planeLane < 1)
+                store.dispatch({ type: ActionType.SET_LANE, value: planeLane + 1 });
         }
     });
 }
